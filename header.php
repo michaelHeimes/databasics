6f460013c8d3fe6b82c7824cededb3b7d8f62579
Michael Heimes <michael@cairndigitalmedia.com>
2021-05-14 14:04:55 -0400

    GTM code and debugging

--- header.php
+++ header.php
@@ -20,19 +20,11 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<!-- Google Tag Manager -->
-	<script>(function ( w, d, s, l, i ) {
-      w[l] = w[l] || []
-      w[l].push( {
-        'gtm.start':
-          new Date().getTime(), event: 'gtm.js',
-      } )
-      var f = d.getElementsByTagName( s )[0],
-        j = d.createElement( s ), dl = l != 'dataLayer' ? '&l=' + l : ''
-      j.async = true
-      j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-      f.parentNode.insertBefore( j, f )
-    })( window, document, 'script', 'dataLayer', 'GTM-MFRZJSD' )</script>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MFRZJSD');</script>
 	<!-- End Google Tag Manager -->
 
 	<!-- Lucky Orange -->
@@ -57,6 +49,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+	
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFRZJSD" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'databasics'); ?></a>
